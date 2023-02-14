9e9ffba89ffb374673cc8c9336da166f4980d6e9
asupryhan <ponalyon@gmail.com>
2023-02-14 22:53:36 +0200

    init leetcode solution

---
+++ problems/ValidParentheses.py
@@ -0,0 +1,42 @@
+class ValidParentheses(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        cor = True
+        for i in s:
+            if i == '(':
+                stack.append('(')
+            elif i == ')':
+                if len(stack) == 0 or stack[-1] != '(':
+                    cor = False
+                    break
+                elif stack[-1] == '(':
+                    stack.pop()
+            if i == '[':
+                stack.append('[')
+            elif i == ']':
+                if len(stack) == 0 or stack[-1] != '[':
+                    cor = False
+                    break
+                elif stack[-1] == '[':
+                    stack.pop()
+            if i == '{':
+                stack.append('{')
+            elif i == '}':
+                if len(stack) == 0 or stack[-1] != '{':
+                    cor = False
+                    break
+                elif stack[-1] == '{':
+                    stack.pop()
+        if (cor and len(stack) == 0):
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    sol = ValidParentheses()
+    print(sol.isValid("({[(]})"), "expected False")
